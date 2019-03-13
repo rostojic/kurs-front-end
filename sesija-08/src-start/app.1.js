09f3c4a6c379c99630e27b041f2844cd7b897a5c
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2019-03-13 17:14:23 +0100

    sesija-08 pripema fajlova

---
+++ sesija-08/src-start/app.1.js
@@ -0,0 +1,11 @@
+//CH 1 
+//Napisati funkciju  koja prima niz brojeva a vraca njihov zbir
+
+//CH 2
+//Napisati funkciju  koja prima niz brojeva i vraca njihov maximum
+
+//CH 3
+//Napisati funkciju koja prima niz brojeva i vraca najmanji
+
+//CH 4
+//Napisati funkciju koja prima niz (moze i niz stringova), a vraca niz u obrnutom redosledu
