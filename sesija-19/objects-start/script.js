601814b61cfd03fe4c2a0d1bbfbcdce9ff5d6192
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2019-04-10 16:51:16 +0200

    vezbe dodane za sesiju 19

---
+++ sesija-19/objects-start/script.js
@@ -0,0 +1,8 @@
+//Code challenge 1
+
+
+//Code challenge 2
+
+
+//code challenge 3
+  
---
+++ sesija-19/roller-coaster-start/script.js
@@ -0,0 +1,4 @@
+var text = "da li ste zavrsili challenge 1?";
+var div = document.getElementById('rollercoaster');
+var fontSize = 5;
+//hint!!!! span.style.fontSize
