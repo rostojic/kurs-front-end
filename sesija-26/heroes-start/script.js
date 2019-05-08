a283beb9e5f55fd8cf3dcfbeaa715bcb0a007d6e
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2019-05-08 17:21:03 +0200

    vezba super heroji

---
+++ sesija-26/heroes-start/script.js
@@ -0,0 +1,17 @@
+var header = document.querySelector('header');
+var section = document.querySelector('section');
+
+
+function populateHeader(jsonObj) {
+ 
+}
+
+function showHeroes(jsonObj) {
+  
+}
+
+populateHeader(heroes);
+showHeroes(heroes);
+
+//Kreirajte account na https://www.themoviedb.org/
+//zapisite negde vas API key, ili ga posaljite sebi na e-mail.
---
+++ sesija-26/heroes-start/superheroes.js
@@ -0,0 +1,43 @@
+var heroes = {
+  "squadName" : "Code.rs hero squad",
+  "homeTown" : "Novi Beograd",
+  "formed" : 2018,
+  "secretBase" : "Super tower",
+  "active" : true,
+  "members" : [
+    {
+      "name" : "Ljubitelji JavaScripta",
+      "age" : 29,
+      "secretIdentity" : "Dan Jukes",
+      "powers" : [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
+    },
+    {
+      "name" : "CSS Masteri",
+      "age" : 39,
+      "secretIdentity" : "Jane Wilson",
+      "powers" : [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name" : "HTML Eternal Flame",
+      "age" : 1000000,
+      "secretIdentity" : "Unknown",
+      "powers" : [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel",
+        "Copy Paste Super Power"
+      ]
+    }
+  ]
+};
+
