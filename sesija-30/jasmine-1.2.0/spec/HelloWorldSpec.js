390510299dfc87636b72495622b3453240dde1aa
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2019-05-17 19:35:11 +0200

    jasmine added

---
+++ sesija-30/jasmine-1.2.0/spec/HelloWorldSpec.js
@@ -0,0 +1,10 @@
+function helloWorld() {
+    return 'Hello world!';
+}
+
+describe('Hello world', function () {
+    it('says hello', function () {
+      expect(helloWorld()).toEqual('Hello world!');
+    });
+  });
+  
