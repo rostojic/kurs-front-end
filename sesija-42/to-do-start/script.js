1b95e79a748df3fd03b79fd93842203690cce161
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2019-06-19 20:05:16 +0200

    coding challenge

---
+++ sesija-42/to-do-start/script.js
@@ -0,0 +1,8 @@
+/*
+    -Dugme add new to-do dodaje novi li
+    -U li dodati input type checkbox
+    -U li dodati jedan span sa sadrzajem text boxa 
+    -Klikom na check box “precrtati” span sadrzaj (resenje u CSS)
+    -Dugme delete treba da obrise iz liste sve chekirane iteme
+    -Preporuka: koristiti MVC sa proslih casova (nije obavezno!!!)
+*/
