8a2c56234f065aa072ca3672ada7717f0e10fffd
Radovan Ostojic <Radovan.Ostojic@Vanderlande.com>
2019-06-26 18:52:54 +0200

    angjs-hello

---
+++ sesija-45/02-AngularJs-Hello/app.js
@@ -0,0 +1,8 @@
+// MODULE
+var myApp = angular.module('myApp',[]);
+// CONTROLLERS
+myApp.controller('mainController', function($scope){
+    $scope.greeting = "Dobro dosli na mini kurs AngularJS!!!!!";
+});
+
+
